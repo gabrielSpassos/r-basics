40d4a22afde6b9f2b15c02e034ad1b1c6d71d59b
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-11-03 17:21:44 -0300

    :tada: iniciando repo de estudos em r

---
+++ variables_functions.R
@@ -0,0 +1,27 @@
+x <- 5
+y <- 3
+7 -> z
+
+x
+print(z)
+print(paste(x, y, z))
+
+primos <- c(2,3,5,7,11,13,19,23,29)
+
+# Média Aritmética
+mean(primos)
+
+sum(primos)
+
+max(primos)
+
+min(primos)
+
+# Desvio Padrão
+sd(primos)
+
+# Variância
+var(primos)
+
+#mediana
+median(primos)
